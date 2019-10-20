2c394676a60ee147e3258339f67dc5791385ff67
SukandaChanawt1999 <56350041+SukandaChanawt1999@users.noreply.github.com>
2019-10-20 13:14:57 +0700

    8.5 Coding the Calculator

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var result: EditText
+    private lateinit var newNumber: EditText
+    private val displayOption by lazy { findViewById<TextView>(R.id.operation) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

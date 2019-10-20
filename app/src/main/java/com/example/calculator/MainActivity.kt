c35512bac6845241bf05193c782d5ac5368325c9
SukandaChanawt1999 <56350041+SukandaChanawt1999@users.noreply.github.com>
2019-10-20 15:57:39 +0700

    18.1 Add Neg Button to Portrait

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 private const val STATE_PENDING_OPERATION = "PendingOperation"
@@ -14,38 +12,13 @@ private const val STATE_OPERAND1_STORED = "Operand1_Stored"
 
 class MainActivity : AppCompatActivity() {
 
-//    private lateinit var result: EditText
-//    private lateinit var newNumber: EditText
-//    private val displayOperation by lazy(LazyThreadSafetyMode.NONE) { findViewById<TextView>(R.id.operation) }
-
     private var operand1: Double? = null
-
-    private var pendingOperation = "="
+    private var pendingOperation =  "="
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        result = findViewById(R.id.result)
-//        newNumber = findViewById(R.id.newNumber)
-//
-//        val button0: Button = findViewById(R.id.button0)
-//        val button1: Button = findViewById(R.id.button1)
-//        val button2: Button = findViewById(R.id.button2)
-//        val button3: Button = findViewById(R.id.button3)
-//        val button4: Button = findViewById(R.id.button4)
-//        val button5: Button = findViewById(R.id.button5)
-//        val button6: Button = findViewById(R.id.button6)
-//        val button7: Button = findViewById(R.id.button7)
-//        val button8: Button = findViewById(R.id.button8)
-//        val button9: Button = findViewById(R.id.button9)
-//        val buttonDot: Button = findViewById(R.id.buttonDot)
-//
-//        val buttonEquals = findViewById<Button>(R.id.buttonEquals)
-//        val buttonDivide = findViewById<Button>(R.id.buttonDivide)
-//        val buttonMultiply = findViewById<Button>(R.id.buttonMultiply)
-//        val buttonMinus = findViewById<Button>(R.id.buttonMinus)
-//        val buttonPlus = findViewById<Button>(R.id.buttonPlus)
 
         val listener = View.OnClickListener { v ->
             val b = v as Button
@@ -65,37 +38,41 @@ class MainActivity : AppCompatActivity() {
         buttonDot.setOnClickListener(listener)
 
         val opListener = View.OnClickListener { v ->
-            val op = ( v as Button).text.toString()
+            val op = (v as Button).text.toString()
             try {
                 val value = newNumber.text.toString().toDouble()
                 performOperation(value, op)
             }catch (e: NumberFormatException){
                 newNumber.setText("")
             }
+
             pendingOperation = op
             operation.text = pendingOperation
+
         }
         buttonEquals.setOnClickListener(opListener)
         buttonDivide.setOnClickListener(opListener)
         buttonMultiply.setOnClickListener(opListener)
         buttonMinus.setOnClickListener(opListener)
         buttonPlus.setOnClickListener(opListener)
-        buttonNeg.setOnClickListener({view ->
+
+        buttonNeg.setOnClickListener{
             val value = newNumber.text.toString()
-            if (value.isNotEmpty()){
+            if (value.isEmpty()){
                 newNumber.setText("-")
             }else{
                 try {
-                    var doubleValue = value.toDouble();
+                    var doubleValue = value.toDouble()
                     doubleValue *= -1
                     newNumber.setText(doubleValue.toString())
-                }catch (e: NumberFormatException){
+                }catch (e:java.lang.NumberFormatException){
                     newNumber.setText("")
                 }
             }
-        })
+        }
+
     }
-    private fun performOperation(value: Double, operation: String){
+    private fun performOperation( value: Double, operation: String){
         if (operand1 == null){
             operand1 = value
         }else{
@@ -103,11 +80,12 @@ class MainActivity : AppCompatActivity() {
             if (pendingOperation == "="){
                 pendingOperation = operation
             }
-            when(pendingOperation){
+
+            when (pendingOperation){
                 "=" -> operand1 = value
-                "/" -> operand1 = if (value == 0.0){
+                "/" -> operand1=if (value == 0.0){
                     Double.NaN
-                }else{
+                }else {
                     operand1!! / value
                 }
                 "*" -> operand1 = operand1!! * value
@@ -123,23 +101,24 @@ class MainActivity : AppCompatActivity() {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         if (operand1 != null){
-            outState.putDouble(STATE_OPERAND1,operand1!!)
-            outState.putBoolean(STATE_OPERAND1_STORED,true)
+            outState.putDouble(STATE_OPERAND1, operand1!!)
+            outState.putBoolean(STATE_OPERAND1_STORED, true)
         }
         outState.putString(STATE_PENDING_OPERATION, pendingOperation)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        operand1 = if (savedInstanceState.getBoolean(STATE_OPERAND1_STORED,false)){
+
+        operand1 = if (savedInstanceState.getBoolean(STATE_OPERAND1_STORED, false)){
             savedInstanceState.getDouble(STATE_OPERAND1)
         }else{
             null
         }
-        operand1 = savedInstanceState.getDouble(STATE_OPERAND1)
 
-        pendingOperation = savedInstanceState.getString(STATE_PENDING_OPERATION).toString()
+
+        pendingOperation =savedInstanceState.getString(STATE_PENDING_OPERATION).toString()
         operation.text = pendingOperation
     }
-
 }
+

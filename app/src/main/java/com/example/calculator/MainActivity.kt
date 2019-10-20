08f37a07710d4ccd08807acb16e56f47f9e72116
SukandaChanawt1999 <56350041+SukandaChanawt1999@users.noreply.github.com>
2019-10-20 13:07:40 +0700

    8.1 Coding the Calculator

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private var result: EditText = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
